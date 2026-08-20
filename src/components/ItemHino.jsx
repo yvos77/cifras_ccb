d93c042ef4d87f08d10693d50dd347b81c729ecc
yvos77 <yuripb902@gmail.com>
2026-08-20 10:20:52 -0300

    feat: Adiciona navegação dos hinos.

--- src/components/ItemHino.jsx
+++ src/components/ItemHino.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 function ItemHino({ hino }) {
     return (
         <li>
+            <Link to={`/hino/${hino.slug}`}>
             {hino.numero}{hino.numero && <span> - </span>}{hino.nome}
+            </Link>
         </li>
     );
 }
