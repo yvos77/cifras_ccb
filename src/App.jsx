74bdff1fbab94eff1647bbab27f732b7bdf097f7
yvos77 <yuripb902@gmail.com>
2026-08-19 23:19:19 -0300

    feat: camada de acesso aos dados dos hinos

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+import { useState, useEffect } from "react";
 import { listarHinos } from "./data/repositorio";
 
 function App() {
-  listarHinos().then((hinos) => console.log(hinos));
-  return <h1>Catálogo</h1>;
+  const [hinos, setHinos] = useState([]);
+
+  useEffect(() => {
+    listarHinos().then((resultado) => setHinos(resultado));
+  }, []);
+
+  return (
+    <div>
+      <h1>Cifras CCB</h1>
+      <ul>
+        {hinos.map((hino) => (
+          <li key={hino.id}>
+            {hino.numero}{hino.numero && <span> - </span>}{hino.nome}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
