5b532d20d4d85c867f8fb2bd378b96023e582b56
yvos77 <yuripb902@gmail.com>
2026-08-25 17:30:23 -0300

    fix: corrige espacamento de acorde em palavra partida

--- src/components/LinhaCifra.jsx
+++ src/components/LinhaCifra.jsx
@@ -9,7 +9,9 @@ function LinhaCifra({ linha }) {
         <div className="linha-cifra">
             {pedacos.map((pedaco, i) => (
                 <span className="pedaco" key={i}>
-                    <span className="pedaco-acorde">{pedaco.acorde}</span>
+                    <span className={pedaco.colado ? "pedaco-acorde colado" : "pedaco-acorde"}>
+                        {pedaco.acorde}
+                    </span>
                     <span className="pedaco-letra">{pedaco.texto}</span>
                 </span>
             ))}
--- src/utils/cifra.js
+++ src/utils/cifra.js
@@ -1,16 +1,18 @@
 export function separarAcordes(linha) {
-
     const partes = linha.split(/\[([^\]]+)\]/);
     const pedacos = [];
 
+    function adicionar(acorde, texto) {
+        pedacos.push({ acorde, texto, colado: !texto.endsWith(" ") });
+    }
+
     if (partes[0] !== "") {
-        pedacos.push({ acorde: null, texto: partes[0] });
+        adicionar(null, partes[0]);
     }
 
     for (let i = 1; i < partes.length; i += 2) {
-        pedacos.push({ acorde: partes[i], texto: partes[i + 1] ?? "" });
+        adicionar(partes[i], partes[i + 1] ?? "");
     }
 
     return pedacos;
-    
 }
